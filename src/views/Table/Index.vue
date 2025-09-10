0ca90b8415aae1dbfdfde1cbc9f44db4466815d4
mbozkurt020 <mehmetbozkurt@plusclouds.com>
2025-09-11 01:15:06 +0300

    sa

--- src/views/Table/Index.vue
+++ src/views/Table/Index.vue
@@ -7,6 +7,7 @@
         :is-show-move-button="false"
       />
       <PBarMobile
+          v-if="device.mobile"
         :nav-items="areas"
         @clickSidebarItem="handleClickSidebarItem"
         :selected-index="tableDetailStore.tableSelectedIndex"
